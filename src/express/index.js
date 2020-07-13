4552a9f0c72ba72480b9271a6fa4603edd6d3b3d
bgutsol <bogdan.gutsol@gmail.com>
2020-07-13 21:28:52 +0300

    fix: remove comments

--- src/express/index.js
+++ src/express/index.js
@@ -2,22 +2,16 @@
 
 const express = require(`express`);
 
-// Маршруты приложения мы опишем в отдельных файлах.
-// Для определения маршрутов мы воспользуемся Router().
-// Примеры маршрутов будут продемонстрированы ниже по тексту.
 const offersRoutes = require(`./routes/offers-routes`);
 const myRoutes = require(`./routes/my-routes`);
 const mainRoutes = require(`./routes/main-routes`);
 
-// Зафиксируем порт для сервера
 const DEFAULT_PORT = 8080;
 
 const app = express();
 
-// Подключим созданные маршруты
 app.use(`/offers`, offersRoutes);
 app.use(`/my`, myRoutes);
 app.use(`/`, mainRoutes);
 
-// Запуск сервера
 app.listen(DEFAULT_PORT);
